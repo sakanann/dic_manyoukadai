c5e2fa0b67ddc9aed8de4fc282cd89cc6cb3f260
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-09 19:11:05 +0900

    マイグレーション redo実行

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 2023_01_09_084117) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+    t.string "title", null: false
+    t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
