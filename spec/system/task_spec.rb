01ae730ef7a3e7c00237dd9e25c467f1c17970bd
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-13 02:45:11 +0900

    system_specに一覧表示、作成順追加

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
+
+  let!(:task) { FactoryBot.create(:task, title: 'task') }
+
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
@@ -17,6 +20,14 @@ RSpec.describe 'タスク管理機能', type: :system do
         task = FactoryBot.create(:task, title: 'task1')
         visit tasks_path
         expect(page).to have_content 'task1'
+      end
+    end
+
+    context 'タスクが作成日時の降順に並んでいる場合' do
+      it '新しいタスクが一番上に表示される' do
+        visit tasks_path
+        task_list = all('.task_row')
+        expect(task_list.first).to have_content 'task'
       end
     end
   end
