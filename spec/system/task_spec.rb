42990ac88b439d78194e6957b7ceb022702aa8d5
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-12 16:01:51 +0900

    システムスペック実装

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タスクを新規作成した場合' do
       it '作成したタスクが表示される' do
         visit new_task_path
-        fill_in 'タイトル', with: 'test'
-        fill_in '内容', with: 'test'
-        click_button '登録する'
+        fill_in 'task[title]', with: 'test'
+        fill_in 'task[content]', with: 'test'
+        click_on '登録する'
         expect(page).to have_content 'test'
       end
     end
@@ -19,21 +19,13 @@ RSpec.describe 'タスク管理機能', type: :system do
         expect(page).to have_content 'task1'
       end
     end
-    context 'タスクが作成日時の降順に並んでいる場合' do
-      it '新しいタスクが一番上に表示される' do
-        task = FactoryBot.create(:task, title: 'task1')
-        visit tasks_path
-        task_desc_test = all('td').first
-        expect(task_desc_test).to have_content 'task1'
-      end
-    end
   end
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        task = FactoryBot.create(:task)
+        task = FactoryBot.create(:task, title: 'task', content: 'task')
         visit task_path(task)
-        expect(page).to have_content 'test'
+        expect(page).to have_content 'task'
       end
     end
   end
