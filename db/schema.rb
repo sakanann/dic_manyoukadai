34aa8ee57bb29d17359d9e1fc82ceef037605230
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-15 21:20:13 +0900

    終了期限カラム　制約とデフォルト値設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_14_173704) do
+ActiveRecord::Schema.define(version: 2023_01_15_111748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,6 @@ ActiveRecord::Schema.define(version: 2023_01_14_173704) do
     t.text "content", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "expired_at"
+    t.datetime "expired_at", default: -> { "now()" }, null: false
   end
 end
