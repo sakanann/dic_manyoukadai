cbaf626db13a9fba9ac9360ad04e2c8fc078e52f
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-21 03:12:55 +0900

    priorityカラムとenum実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_19_053557) do
+ActiveRecord::Schema.define(version: 2023_01_20_172804) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,5 +22,7 @@ ActiveRecord::Schema.define(version: 2023_01_19_053557) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "expired_at", default: -> { "now()" }, null: false
     t.integer "status", default: 0, null: false
+    t.integer "priority", default: 0, null: false
   end
+
 end
