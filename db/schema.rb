85e2466df9ca5e03ef66d04f1b05db85ef8db0f2
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-25 02:49:46 +0900

    外部キー設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_01_24_155004) do
+ActiveRecord::Schema.define(version: 2023_01_24_174045) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 2023_01_24_155004) do
     t.datetime "expired_at", default: -> { "now()" }, null: false
     t.integer "status", null: false
     t.integer "priority", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,5 @@ ActiveRecord::Schema.define(version: 2023_01_24_155004) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "tasks", "users"
 end
