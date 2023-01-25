86548fdfbe5634ddd7b6b5fec3a33743ff81d851
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-25 15:42:06 +0900

    最初のユーザをseedで作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  email: "first@first.com",
+  name: "first",
+  passward_digest: "first"
+)
