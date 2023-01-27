a3d8990d2680570703d280623597b061a14ccbcb
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-27 13:42:20 +0900

    ログインユーザーの投稿だけを表示index notice削除

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks
 
     if params[:sort_expired_at]
       @tasks = @tasks.all.sort_expired_at.page(params[:page])
