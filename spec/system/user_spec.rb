bd699ff11e3adf2797a4acee35ef1b311b629c08
takuya sakamoto <sthtcwm@yahoo.co.jp>
2023-01-28 23:53:55 +0900

    テスト項目#2-3&4 ひとまず通った

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'ユーザー管理機能' , type: :system do
-
+  let(:user1) { FactoryBot.create(:user1) }
+  let(:user2) { FactoryBot.create(:user2) }
 
   describe 'ユーザ登録のテスト' do
     context '新規登録画面に遷移した場合' do
@@ -33,10 +34,31 @@ RSpec.describe 'ユーザー管理機能' , type: :system do
         fill_in 'user_password', with: 'sakamoto'
         fill_in 'user_password_confirmation', with: 'sakamoto'
         click_on "登録"
-        sleep(5)
         expect(page).to have_content '坂本のページ'
       end
-      
+    end
+
+    context '一般ユーザが他人の詳細画面に飛んだ場合' do
+      it 'タスク一覧画面に遷移する' do
+        visit new_session_path
+        fill_in 'session[email]', with: user1.email
+        fill_in 'session[password]', with: user1.password
+        click_on "Log in"
+        sleep(1)
+        visit user_path(user2)
+        expect(current_path).to eq tasks_path
+      end
+    end
+
+    context 'ログイン後' do
+      it 'ログアウトができる' do
+      visit new_session_path
+      fill_in 'session_email', with: user1.email
+      fill_in 'session_password', with: user1.password
+      click_on "Log in"
+      click_on "Logout"
+      expect(current_path).to eq new_session_path
+      end
     end
   end
 end
